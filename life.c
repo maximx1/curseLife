63bc725014a027bc4696468fcb407715d10aff16
Justin <walrathjaw@gmail.com>
2013-04-23 21:56:03 -0400

    Got the threading up and running(although it is useless since c handles the gol well enough in ncurses.

--- life.c
+++ life.c
@@ -1,20 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ncurses.h>
+#include<pthread.h>
 #include<unistd.h>
 #include<time.h>
 
 #define MAX 40
+#define MAXTHREADS 2
 
 typedef char * string;
 
+typedef struct _colony
+{
+	int **arr1;
+	int **arr2;
+	int start;
+	int end;
+} colony;
+
 void run(int numbersOn);
-void print(int buf[][MAX], int numbersOn);
+void print(int **buf, int numbersOn);
 void init_screen();
 void kill_screen();
-void calcNext(int arr1[][MAX], int arr2[][MAX]);
 int a(int i);
 int s(int i);
+
+//Thread function
+void *calcNextGen(void *th);
 
 int main(int args, string argv[])
 {
@@ -33,19 +45,28 @@ int main(int args, string argv[])
 	return(0);
 }
 
+//This is the function that initializes everything and then loops through the generations.
 void run(int numbersOn)
 {
-	int arr1[MAX][MAX];
-	int arr2[MAX][MAX];
-	int neighbors[8];
+	int i, j;
+	int **arr1 = (int **)malloc(sizeof(int *) * MAX);	//Create 2 arrays of arrays with 20 rows
+	int **arr2 = (int **)malloc(sizeof(int *) * MAX);
+
+	//Gonna just deal with 2 threads
+	colony colonies[MAXTHREADS];
+	pthread_t thread[MAXTHREADS];
+
+	for(i = 0; i < MAX; i++)
+	{
+		arr1[i] = (int *)malloc(sizeof(int) * MAX);		//Create 20 columns per each row 
+		arr2[i] = (int *)malloc(sizeof(int) * MAX);		//Create 20 columns per each row 
+	}
 
 	srand(time(NULL));
 
 	int ranx = rand() % (MAX - 4) + 4;
 	int rany = rand() % (MAX - 4) + 4;
 
-	int i;
-	int j;
 	for(i = 0; i < MAX; i++)
 	{
 		for(j = 0; j < MAX; j++)
@@ -64,6 +85,16 @@ void run(int numbersOn)
 	}
 	print(arr1, numbersOn);
 
+	//Split up the data into multiple threads.
+	int split = MAX / MAXTHREADS;
+
+	for(i = 0; i < MAXTHREADS; i++)
+	{
+		colonies[i].arr1 = arr1;
+		colonies[i].arr2 = arr2;
+		colonies[i].start = i * split;
+		colonies[i].end = (i + 1) * split;
+	}
 
 //321 
 //4x0
@@ -79,53 +110,77 @@ void run(int numbersOn)
 				arr2[i][j] = arr1[i][j];
 			}
 		}
-		for(i = 0; i < MAX; i++)
+	
+		//Start the 2 threads
+		for(i = 0; i < MAXTHREADS; i++)
 		{
-			for(j = 0; j < MAX; j++)
-			{
-				neighbors[0] = arr2[i][a(j+1)];
-				neighbors[1] = arr2[s(i-1)][a(j+1)];
-				neighbors[2] = arr2[s(i-1)][j];
-				neighbors[3] = arr2[s(i-1)][s(j-1)];
-				neighbors[4] = arr2[i][a(j-1)];
-				neighbors[5] = arr2[a(i+1)][s(j-1)];
-				neighbors[6] = arr2[a(i+1)][j];
-				neighbors[7] = arr2[a(i+1)][a(j+1)];
-
-				int k;
-				int count = 0;
-				for(k = 0; k < 8; k++)
-				{
-					if(neighbors[k] == 1)
-					{
-						count++;
-					}
-				}
-				
-				if(arr1[i][j] == 1 && count < 2)
-				{//rule 1
-					arr1[i][j] = 0;
-				}
-				else if(arr1[i][j] == 1 && count <= 3 && count >= 2)
-				{//rule 2
-					arr1[i][j] = 1;
-				}
-				else if(arr1[i][j] == 1 && count > 3)
-				{//rule 3
-					arr1[i][j] = 0;
-				}
-				else if(arr1[i][j] == 0 && count == 3)
-				{//rule 4
-					arr1[i][j] = 1;
-				}
-			}
+			pthread_create(&thread[i], NULL, calcNextGen, &colonies[i]);
 		}
 
+		//Close the 2 threads
+		for(i = 0; i < MAXTHREADS; i++)
+		{
+			pthread_join(thread[i], NULL);
+		}
+
+		//Print out the generation
 		print(arr1, numbersOn);
 	}
 }
 
-//Quickfunction to test edge
+/**
+ * This function is passed into the thread to run along with the struct of data to manipulate.
+ */
+void *calcNextGen(void *th)
+{
+	colony* data = (colony*)th;
+	int i, j, k, count;
+	
+	int neighbors[8];
+	
+	for(i = data->start; i < data->end; i++)
+	{
+		for(j = 0; j < MAX; j++)
+		{
+			neighbors[0] = data->arr2[i][a(j+1)];
+			neighbors[1] = data->arr2[s(i-1)][a(j+1)];
+			neighbors[2] = data->arr2[s(i-1)][j];
+			neighbors[3] = data->arr2[s(i-1)][s(j-1)];
+			neighbors[4] = data->arr2[i][a(j-1)];
+			neighbors[5] = data->arr2[a(i+1)][s(j-1)];
+			neighbors[6] = data->arr2[a(i+1)][j];
+			neighbors[7] = data->arr2[a(i+1)][a(j+1)];
+
+			count = 0;
+			for(k = 0; k < 8; k++)
+			{
+				if(neighbors[k] == 1)
+				{
+					count++;
+				}
+			}
+				
+			if(data->arr1[i][j] == 1 && count < 2)
+			{//rule 1
+				data->arr1[i][j] = 0;
+			}
+			else if(data->arr1[i][j] == 1 && count <= 3 && count >= 2)
+			{//rule 2
+				data->arr1[i][j] = 1;
+			}
+			else if(data->arr1[i][j] == 1 && count > 3)
+			{//rule 3
+				data->arr1[i][j] = 0;
+			}
+			else if(data->arr1[i][j] == 0 && count == 3)
+			{//rule 4
+				data->arr1[i][j] = 1;
+			}
+		}
+	}
+}
+
+//Quick function to test Max edges
 int a(int i)
 {
 	if(i > MAX - 1)
@@ -135,6 +190,7 @@ int a(int i)
 	return(i);
 }
 
+//Quick function to test the 0 edges
 int s(int i)
 {
 	if(i < 0)
@@ -144,7 +200,8 @@ int s(int i)
 	return(i);
 }
 
-void print(int buf[][MAX], int numbersOn)
+//Prints out everything in the array
+void print(int **buf, int numbersOn)
 {
 	clear();
 	int i;
@@ -186,6 +243,7 @@ void print(int buf[][MAX], int numbersOn)
 	sleep(1);
 }
 
+//Initializes the ncurses screen
 void init_screen()
 {
 	initscr();
@@ -196,6 +254,7 @@ void init_screen()
 	init_pair(1, COLOR_BLACK, COLOR_GREEN);
 }
 
+//Closes up all the ncurses resources
 void kill_screen()
 {
 	getchar();
