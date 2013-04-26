8dd3b39456ac55ab2a1b8c7bc994a8faff3d48a0
Justin Walrath <walrathjaw@gmail.com>
2013-04-25 20:53:58 -0400

    Got most command line flags up and running as well as the ncurses screen size register.

--- life.c
+++ life.c
@@ -5,11 +5,13 @@
 #include<unistd.h>
 #include<time.h>
 
-#define MAX 70
-#define MAXTHREADS 2
+#define wait 150000
+#define MAXPRESETS 1
 
+//Makes strings easier to understand
 typedef char * string;
 
+//data handler for the threads
 typedef struct _colony
 {
 	int **arr1;
@@ -18,83 +20,139 @@ typedef struct _colony
 	int end;
 } colony;
 
-void run(int numbersOn);
-void print(int **buf, int numbersOn);
+//Settings
+int row, col, numbersOn, MAXTHREADS;
+int presetDesigns[MAXPRESETS];
+
+void runLife();
+void print(int **buf);
 void init_screen();
 void kill_screen();
-int a(int i);
-int s(int i);
+int ai(int i);
+int si(int i);
+int aj(int i);
+int sj(int i);
 
 //Thread function
 void *calcNextGen(void *th);
 
 int main(int args, string argv[])
 {
-	int numbersOn = 0;
+	int i = 0;
+	row = col = numbersOn = 0;
+	MAXTHREADS = 2;
+
+	for(i = 0; i < MAXPRESETS; i++)
+	{
+		presetDesigns[i] = 0;
+	}
 
 	if(args > 1)
 	{
-		if(strcmp("-n", argv[1]) == 0)
+		for(i = 1; i < args; i++)
 		{
-			numbersOn = 1;
+			//Tells the print function to display the cell values
+			if(strcmp("-n", argv[i]) == 0)
+			{
+				numbersOn = 1;
+			}
+
+			//Read number of threads from the command line.
+			if(strcmp("-t", argv[i]) == 0 || strcmp("--threads", argv[i]) == 0)
+			{
+				MAXTHREADS = atoi((char *)argv[++i]);
+				printf("%s\n", argv[i]);
+				continue;
+			}
+			
+			//Tells the program to preload the glider.
+			if(strcmp("-g", argv[i]) == 0)
+			{
+				presetDesigns[0] = 1;
+			}
 		}
 	}
 	init_screen();
-	run(numbersOn);
+	runLife();
 	kill_screen();
 	return(0);
 }
 
 //This is the function that initializes everything and then loops through the generations.
-void run(int numbersOn)
+void runLife()
 {
 	int i, j;
-	int **arr1 = (int **)malloc(sizeof(int *) * MAX);	//Create 2 arrays of arrays with 20 rows
-	int **arr2 = (int **)malloc(sizeof(int *) * MAX);
+	i = j = 0;
 
-	//Gonna just deal with 2 threads
-	colony colonies[MAXTHREADS];
-	pthread_t thread[MAXTHREADS];
+	//Get the screen size
+	getmaxyx(stdscr, row, col);
 
-	for(i = 0; i < MAX; i++)
+	//Cut off the edge to make things run infinitely.
+	--row;
+	printw("Row: %d, Col: %d", --row, --col);
+	refresh();
+	sleep(2);
+
+	//Makes sure that the number of threads are less than the amount of data to actually be able to be processed.
+	if(MAXTHREADS > row / 2)
 	{
-		arr1[i] = (int *)malloc(sizeof(int) * MAX);		//Create 20 columns per each row 
-		arr2[i] = (int *)malloc(sizeof(int) * MAX);		//Create 20 columns per each row 
+		MAXTHREADS = row / 2;
 	}
 
-	srand(time(NULL));
+	//Initiate rows
+	int **arr1 = (int **)malloc(sizeof(int *) * row);	//Create 2 arrays of arrays that represent rows
+	int **arr2 = (int **)malloc(sizeof(int *) * row);
 
-	int ranx = rand() % (MAX);
-	int rany = rand() % (MAX);
-
-	for(i = 0; i < MAX; i++)
+	//Initiate columns
+	for(i = 0; i < row; i++)
 	{
-		for(j = 0; j < MAX; j++)
+		arr1[i] = (int *)malloc(sizeof(int) * col);		//Create columns per each row 
+		arr2[i] = (int *)malloc(sizeof(int) * col);		//Create columns per each row 
+	}
+
+	//default 2 threads but the user can change it.
+	colony *colonies;
+	colonies = (colony *)malloc(sizeof(colony) * MAXTHREADS);
+	pthread_t *thread;
+	thread = (pthread_t *)malloc(sizeof(pthread_t) * MAXTHREADS);
+
+	//Initialize the array to 0
+	for(i = 0; i < row; i++)
+	{
+		for(j = 0; j < col; j++)
 		{
 			arr1[i][j] = arr2[i][j] = 0;
 		}
 	}
 
-	//Initialize the startingpoints
-	for(i = MAX * MAX / 2; i > 0; i--)
+	if(presetDesigns[0])
 	{
-		arr1[rany][ranx] = 1;
-		ranx = rand() % (MAX - 4) + 4;
-		rany = rand() % (MAX - 4) + 4;	
+		//This is for making a glider that runs from {0,0} to {row,row}
+		arr1[0][1] = 1;
+		arr1[1][2] = 1;
+		arr1[2][0] = 1;
+		arr1[2][1] = 1;
+		arr1[2][2] = 1;
+	}
+	else
+	{
+		//Randomly design a seed for the game
+		srand(time(NULL));
+		int ranx = rand() % (col);
+		int rany = rand() % (row);
+		for(i = row * col / 2; i > 0; i--)
+		{
+			arr1[rany][ranx] = 1;
+			ranx = rand() % (col);
+			rany = rand() % (row);	
+		}
 	}
 
-//This is for making a glider
-//	arr1[0][1] = 1;
-//	arr1[1][2] = 1;
-//	arr1[2][0] = 1;
-//	arr1[2][1] = 1;
-//	arr1[2][2] = 1;
-
-	print(arr1, numbersOn);
+	//Print the base 
+	print(arr1);
 
 	//Split up the data into multiple threads.
-	int split = MAX / MAXTHREADS;
-
+	int split = row / MAXTHREADS;
 	for(i = 0; i < MAXTHREADS; i++)
 	{
 		colonies[i].arr1 = arr1;
@@ -103,16 +161,14 @@ void run(int numbersOn)
 		colonies[i].end = (i + 1) * split;
 	}
 
-//321 
-//4x0
-//567
+	//The main game loop
 	int gameLife;
 	for(gameLife = 0; gameLife <1000; gameLife++)
 	{
 		//Copy old array over to the storage array.
-		for(i = 0; i < MAX; i++)
+		for(i = 0; i < row; i++)
 		{
-			for(j = 0; j < MAX; j++)
+			for(j = 0; j < col; j++)
 			{
 				arr2[i][j] = arr1[i][j];
 			}
@@ -131,7 +187,7 @@ void run(int numbersOn)
 		}
 
 		//Print out the generation
-		print(arr1, numbersOn);
+		print(arr1);
 	}
 }
 
@@ -140,24 +196,31 @@ void run(int numbersOn)
  */
 void *calcNextGen(void *th)
 {
+	//Dereference the struct that is passed in
 	colony* data = (colony*)th;
 	int i, j, k, count;
 	
-	int neighbors[8];
+	int neighbors[8];		//A temporary map for the surrounding cells of a 'pixel'
 	
+	//Iterate through all of the cells that the thread is to handle
 	for(i = data->start; i < data->end; i++)
 	{
-		for(j = 0; j < MAX; j++)
+		for(j = 0; j < col; j++)
 		{
-			neighbors[0] = data->arr2[i][a(j+1)];
-			neighbors[1] = data->arr2[s(i-1)][a(j+1)];
-			neighbors[2] = data->arr2[s(i-1)][j];
-			neighbors[3] = data->arr2[s(i-1)][s(j-1)];
-			neighbors[4] = data->arr2[i][a(j-1)];
-			neighbors[5] = data->arr2[a(i+1)][s(j-1)];
-			neighbors[6] = data->arr2[a(i+1)][j];
-			neighbors[7] = data->arr2[a(i+1)][a(j+1)];
+			//Placement of the neighbors is as followed.
+			// 3 2 1 
+			// 4 x 0
+			// 5 6 7
+			neighbors[0] = data->arr2[i][aj(j+1)];
+			neighbors[1] = data->arr2[si(i-1)][aj(j+1)];
+			neighbors[2] = data->arr2[si(i-1)][j];
+			neighbors[3] = data->arr2[si(i-1)][sj(j-1)];
+			neighbors[4] = data->arr2[i][aj(j-1)];
+			neighbors[5] = data->arr2[ai(i+1)][sj(j-1)];
+			neighbors[6] = data->arr2[ai(i+1)][j];
+			neighbors[7] = data->arr2[ai(i+1)][aj(j+1)];
 
+			//Count the living cells around the cell.
 			count = 0;
 			for(k = 0; k < 8; k++)
 			{
@@ -166,31 +229,33 @@ void *calcNextGen(void *th)
 					count++;
 				}
 			}
-				
+			
+			//Applies the rules to the cell
 			if(data->arr1[i][j] == 1 && count < 2)
-			{//rule 1
+			{//rule 1: x<2 dies of starvation
 				data->arr1[i][j] = 0;
 			}
 			else if(data->arr1[i][j] == 1 && count <= 3 && count >= 2)
-			{//rule 2
+			{//rule 2: 2<=x<=3 stays alive.
 				data->arr1[i][j] = 1;
 			}
 			else if(data->arr1[i][j] == 1 && count > 3)
-			{//rule 3
+			{//rule 3: x>3 dies of overcrowding.
 				data->arr1[i][j] = 0;
 			}
 			else if(data->arr1[i][j] == 0 && count == 3)
-			{//rule 4
+			{//rule 4: If dead and there is exactly 3 living neighbors, comes to life as if reproduction.
 				data->arr1[i][j] = 1;
 			}
 		}
 	}
 }
 
+//Handles calculating the row's bottom and top edges
 //Quick function to test Max edges
-int a(int i)
+int ai(int i)
 {
-	if(i > MAX - 1)
+	if(i > row - 1)
 	{
 		return(0);
 	}
@@ -198,25 +263,46 @@ int a(int i)
 }
 
 //Quick function to test the 0 edges
-int s(int i)
+int si(int i)
 {
 	if(i < 0)
 	{
-		return(MAX - 1);
+		return(row - 1);
+	}
+	return(i);
+}
+
+//Handles calculating the column's left and right edges
+//Quick function to test Max edges
+int aj(int i)
+{
+	if(i > col - 1)
+	{
+		return(0);
+	}
+	return(i);
+}
+
+//Quick function to test the 0 edges
+int sj(int i)
+{
+	if(i < 0)
+	{
+		return(col - 1);
 	}
 	return(i);
 }
 
 //Prints out everything in the array
-void print(int **buf, int numbersOn)
+void print(int **buf)
 {
 	clear();
 	int i;
 	int j;
 
-	for(i = 0; i < MAX; i++)
+	for(i = 0; i < row; i++)
 	{
-		for(j = 0; j < MAX; j++)
+		for(j = 0; j < col; j++)
 		{
 			if(buf[i][j] == 1)
 			{
@@ -242,12 +328,11 @@ void print(int **buf, int numbersOn)
 					printw(" ");
 				}
 			}
-			printw(" ");
 		}
-		printw("\n");
+		printw("\n\r");
 	}
 	refresh();
-	usleep(100000);
+	usleep(wait);
 }
 
 //Initializes the ncurses screen
@@ -255,7 +340,7 @@ void init_screen()
 {
 	initscr();
 	start_color();
-	cbreak();
+	nocbreak();
 	noecho();
 
 	init_pair(1, COLOR_BLACK, COLOR_GREEN);
